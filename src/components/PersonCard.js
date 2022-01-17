edb18c45f8879ee7b36eac7b027a929806124f7a
Eavilesz <ernesto-av@hotmail.com>
2022-01-16 20:56:46 -0500

    Tarea poniendo todo junto, con boton de sumar los años

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
 
 class PersonCard extends Component {
+  increaseAge = () => {
+    const count = this.state.personAge + 1;
+    this.setState({ personAge: count });
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      personAge: this.props.age,
+    };
+  }
+
   render() {
-    const { firstName, lastName, age, hairColor } = this.props;
+    const { firstName, lastName, hairColor } = this.props;
     return (
       <div>
         <h1>
           {lastName},{firstName}
         </h1>
-        <p>Age: {age}</p>
+        <p>Age: {this.state.personAge}</p>
         <p>Hair Color: {hairColor}</p>
-        {/* Mi nombre es {name} y mi apellido es {lastName} */}
+        <button onClick={this.increaseAge}>
+          Birthday button for {lastName}, {firstName}
+        </button>
       </div>
     );
   }
